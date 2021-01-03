d0b62a4308c0c6c4a9bd1d8b1d2aeb3ea925287d
kwanghoon.kim <kk03187693@gmail.com>
2021-01-03 22:57:19 +0900

    add 괄호 위치 찾기

---
+++ codingInterview/src/main/java/스택/괄호_위치_찾기.java
@@ -0,0 +1,47 @@
+package 스택;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+* - 문제 설명
+* 괄호 수식과 여는 괄호의 위치가 주어졌을 때 그에 대응하는 닫힌 괄호의 위치를 찾는 코드를 작성하라.
+*
+* - 조건
+* 중복되는 괄호는 없다 ex) [ ( ) ( ) ]
+*/
+public class 괄호_위치_찾기 {
+    public static void main(String[] args) {
+        assertThat(solution("[{1+2*(2+2)}-(1-3)]", 1)).isEqualTo(11);
+    }
+
+    private static int solution(String mathExpression, int position) {
+        if (mathExpression.length() < position) {
+            throw  new IllegalArgumentException("position value is over");
+        }
+
+        Stack<Character> brackets = new Stack<>();
+        char[] chars = mathExpression.toCharArray();
+
+        List<Character> openingBrackets = Arrays.asList('(','{','[');
+        List<Character> closingBrackets = Arrays.asList(')','}',']');
+
+        for (char c : chars) {
+            brackets.push(c);
+        }
+
+        Character positionBracket = brackets.get(position);
+
+        if (openingBrackets.contains(positionBracket)) {
+            throw  new IllegalArgumentException("position value is not openingBracket");
+        }
+
+        int openingBracketIndex = openingBrackets.indexOf(positionBracket);
+        Character targetClosingBracket = closingBrackets.get(openingBracketIndex);
+
+        return brackets.indexOf(targetClosingBracket);
+    }
+}
