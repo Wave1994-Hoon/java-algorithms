05c501fb176a75c8331f5918f6ec2bed3a2f049d
kwanghoon.kim <kk03187693@gmail.com>
2021-01-26 01:42:39 +0900

    add LinkedListCycle

---
+++ leetcode/src/main/java/easy/_141_LinkedListCycle/ListNode.java
@@ -0,0 +1,10 @@
+package easy._141_LinkedListCycle;
+
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+ }
---
+++ leetcode/src/main/java/easy/_141_LinkedListCycle/solution.java
@@ -0,0 +1,28 @@
+package easy._141_LinkedListCycle;
+
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+/*
+* walker: 한 번에 한 칸씩
+* runner: 한 번에 두 칸씩 전진
+*
+* 싸이클이 있으면 둘은 언젠가 만남
+* 싸이클이 없으면 러너거 먼저 null에 닿음
+* 시간 복잡도: O(n)
+* 공간 복잡도: O(1)
+* */
+class Solution {
+g
+}
