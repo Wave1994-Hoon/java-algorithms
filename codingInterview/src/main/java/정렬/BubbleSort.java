4f7c5c96fc6f58ee8b0178e3bb643a7fbde99798
kwanghoon.kim <kk03187693@gmail.com>
2021-02-08 02:41:55 +0900

    add bubble sort java exam

---
+++ codingInterview/src/main/java/정렬/BubbleSort.java
@@ -0,0 +1,39 @@
+package 정렬;
+
+public class BubbleSort {
+    private static void doBubbleSort(int[] array) {
+        doBubbleSort(array, array.length -1);
+    }
+
+    private static void doBubbleSort(int[] array, int lastIndex) {
+        if (lastIndex == 0) {
+            return;
+        }
+        for (int i = 1; i <= lastIndex; i++) {
+            if (array[i - 1] > array[i]) {
+                swap(array, i - 1, i);
+            }
+        }
+        doBubbleSort(array, lastIndex - 1);
+    }
+
+    private static void swap(int[] array, int soruce, int target) {
+        int tmp = array[soruce];
+        array[soruce] = array[target];
+        array[target] = tmp;
+    }
+
+    private static void printArray(int[] array) {
+        for (int data : array) {
+            System.out.print(data + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,0,2,1};
+        printArray(arr);
+        doBubbleSort(arr);
+        printArray(arr);
+    }
+}
